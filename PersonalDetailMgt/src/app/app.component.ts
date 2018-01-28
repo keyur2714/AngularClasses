3603007feca2026efd847f2283e26377cc4117ec
keyur2714 <keyur.555kn@gmail.com>
2018-01-28 13:55:37 +0530

    Added Personal Detail Project

---
+++ PersonalDetailMgt/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  firstName:string = "keyur";
+  lastName:string = "Thakor";
+  panNo:string = "AMUPT4035A";
+  
+}
