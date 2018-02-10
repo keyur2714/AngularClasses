4f45dd939fd0696d1da20fbd189159326c9ce0d1
keyur2714 <keyur.555kn@gmail.com>
2018-02-10 13:57:04 +0530

    Added Grade Component

--- PersonalDetailMgt/src/app/contact/contact.component.ts
+++ PersonalDetailMgt/src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+
+  @Input("mobileNo")
+  mobileNumber : string = '';
+
+  @Input()
+  email : string = '';
 
   constructor() { }
 
